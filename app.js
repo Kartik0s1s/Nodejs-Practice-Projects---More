ebb1697c5c7c3b45e62fb06ae36da929b8e252b8
Kartik Chaudhar <chaudharikartik.com>
2025-10-03 19:57:35 +0530

    Done

---
+++ app.js
@@ -0,0 +1,16 @@
+
+// var fs= require("fs");
+
+// fs.writeFileSync("Kartik.txt", "my name is kartik");
+
+
+// let V1="apple";
+// function fruite(item){
+//     console.log(`item is ${item}`)
+// }
+// fruite("Banana");
+// fruite(V1);
+
+
+
+ 
