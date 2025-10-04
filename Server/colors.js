a3692bccb3d5aa86bb88e05774107e5b6e14adc3
Kartik Chaudhar <chaudharikartik.com>
2025-10-04 17:06:08 +0530

    done for the day

---
+++ Server/colors.js
@@ -0,0 +1,9 @@
+const colors= require("colors");
+
+
+console.log("MY name is Kartik".black);
+console.log("MY name is Kartik".black);
+console.log("MY name is Kartik".black);
+console.log("MY name is Kartik".black);
+console.log("MY name is Kartik".black);
+console.log("MY name is Kartik".black);
--- Server/server.js
+++ Server/server.js
@@ -7,15 +7,16 @@
 //     resp.write("<h1>Hello this is kartik</h1>");
 //     resp.end("Hello");
 // }).listen(3000);
-
+const nodemon= require("nodemon");
 const http = require("http");
+const colors= require("colors");
 
 const server1 = http.createServer((req, resp) => {
   resp.write("<h1>This is server 1</h1>");
   resp.end("We are done with processing");
 });
 server1.listen(4000, () => {
-  console.log("SErve is running");
+  console.log("Serve is running".red);
 });
 
 const server2 = http.createServer((req, resp) => {
@@ -23,5 +24,5 @@ const server2 = http.createServer((req, resp) => {
   resp.end("We are done with processing");
 });
 server2.listen(4001, () => {
-  console.log("SErve is running");
+  console.log("Serve is running".green);
 });
