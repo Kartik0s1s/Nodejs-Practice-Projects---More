6a71f626cb89a9dca56bd1dbbfc6992126e4019f
Kartik Chaudhar <chaudharikartik532@gmail.com>
2025-10-05 17:43:09 +0530

    16 Lectures are over

--- Lecture11_onward/L16_form.js
+++ Lecture11_onward/L16_form.js
@@ -1,45 +1,49 @@
- const http = require("http");
+const http = require("http");
 const fs = require("fs");
+const  querystring = require ('querystring');
 
 const server1 = http.createServer((req, resp) => {
-
- 
   if (req.url == "/") {
-
     fs.readFile("html/form.html", "utf-8", (error, data) => {
-
       if (error) {
-        
         resp.writeHead(500, { "Content-Type": "text/plain" });
         resp.write("Internal Server Error");
         resp.end();
         return;
-      } 
-      
-      else {
+      } else {
         resp.writeHead(200, { "Content-Type": "text/html" });
         resp.write(data);
-        resp.end();  
+        resp.end();
       }
     });
-  }
 
-  
-  else if (req.url == "/submit") {
-     
+
+
+  } else if (req.url == "/submit") {
     resp.writeHead(200, { "Content-Type": "text/html" });
-    resp.write("<h1>DATA SUBMITTED</h1>");
-    resp.end();  
-  }
-
- 
-  else {
     
+    let databody = [];
+    req.on("data", (chunk) => {
+      databody.push(chunk);
+    });
+
+    req.on("end", () => {
+     let rawdata= Buffer.concat(databody).toString();
+     let readable = querystring.parse(rawdata);
+    
+    resp.writeHead(200,{"Contetent-Type":"text/html"})
+    resp.write("<h1>DATA SUBMITTED</h1>");
+    resp.write(`<p>Name: ${readable.Name}</p>`);
+    resp.write(`<p>E-mail: ${readable.email}</p>`);
+    resp.end();
+    });
+        
+
+  } else {
     resp.writeHead(404, { "Content-Type": "text/plain" });
     resp.write("Page Not Found");
     resp.end();
   }
-
 });
 
 server1.listen(3000, () => {
